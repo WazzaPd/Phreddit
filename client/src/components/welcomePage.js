882e587358724c35dcd7a75fb222c69c8811162f
Toni Liang <toni.liang@stonybrook.edu>
2024-11-30 11:20:05 -0500

    implementation of page succcessful

--- client/src/components/welcomePage.js
+++ client/src/components/welcomePage.js
@@ -4,16 +4,29 @@ import LoginPage from "./welcome-page-components/loginPage";
 import RegisterPage from "./welcome-page-components/registerPage";
 
 const WelcomePage = (props) => {
+    const [showRegisterPage, setShowRegisterPage] = useState(false);
+    const [showLoginPage, setShowLoginPage] = useState(false);
 
+    if(showRegisterPage) {
+        return (
+            <RegisterPage setShowRegisterPage = {setShowRegisterPage}/>
+        )
+    }
+
+    if(showLoginPage) {
+        return (
+            <LoginPage setShowLoginPage = {setShowLoginPage} />
+        )
+    }
 
     return (  
         <div className="welcome-page">
             <h1 className="options-container" >Welcome to Phreddit!</h1>
             <div className="options-container">
-                <button className="log-in">
+                <button className="log-in" onClick = {() => setShowLoginPage(true)}>
                     Log in
                 </button>
-                <button className="register">
+                <button className="register" onClick = { () => setShowRegisterPage(true) }>
                     Register Account
                 </button>
                 <button className="continue-as-guest" onClick = { () => props.switchWelcomePageOption() }>
